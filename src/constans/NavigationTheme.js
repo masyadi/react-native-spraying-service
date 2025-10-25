3dec0e931c66739e754f364e2985ae3e76cd3020
_yadi <muchsayadi@gmail.com>
2025-10-25 13:30:03 +0700

    update

--- src/constans/NavigationTheme.js
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from '@react-navigation/native';
-import { COLORS } from './Theme';
-
-export const NavigationTheme = {
-  ...DefaultTheme,
-  colors: {
-    background: COLORS.backgroundColor,
-  },
-};
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Buyer, Home, Komoditas, Order, OrderSuccess, Pestisida, PinLocation, Summary } from "../screens";
 import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from "@react-navigation/native";
-import { COLORS, NavigationTheme } from "../constans";
+  Buyer,
+  Home,
+  Komoditas,
+  Order,
+  OrderSuccess,
+  Pestisida,
+  PinLocation,
+  Summary,
+} from "../screens";
+import { useNavigationContainerRef } from "@react-navigation/native";
+import { COLORS } from "../constans";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,59 +23,56 @@ const Navigation = () => {
   const navigationRef = useNavigationContainerRef();
 
   return (
-    <NavigationContainer theme={NavigationTheme}>
-      <Stack.Navigator
-        independent={true}
-        ref={navigationRef}
-        screenOptions={{
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: COLORS.backgroundColor,
-          },
-        }}
-      >
-        <Stack.Screen
-          name={Routes.ScreenHome}
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="OrderScreen"
-          component={Order}
-          options={{ title: "Lokasi & Waktu" }}
-        />
-        <Stack.Screen
-          name="PinLocationScreen"
-          component={PinLocation}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="KomoditasScreen"
-          component={Komoditas}
-          options={{ title: "Pilih Komoditas" }}
-        />
-        <Stack.Screen
-          name="BuyerScreen"
-          component={Buyer}
-          options={{ title: "Data Pemesan" }}
-        />
-        <Stack.Screen
-          name="SummaryScreen"
-          component={Summary}
-          options={{ title: "Review Pesanan" }}
-        />
-        <Stack.Screen
-          name="OrderSuccessScreen"
-          component={OrderSuccess}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="PestisidaScreen"
-          component={Pestisida}
-          options={{ title: "Pilih Cairan" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      independent={true}
+      ref={navigationRef}
+      screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: { backgroundColor: COLORS.backgroundColor },
+        contentStyle: { backgroundColor: COLORS.backgroundColor },
+      }}
+    >
+      <Stack.Screen
+        name={Routes.ScreenHome}
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="OrderScreen"
+        component={Order}
+        options={{ title: "Lokasi & Waktu" }}
+      />
+      <Stack.Screen
+        name="PinLocationScreen"
+        component={PinLocation}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="KomoditasScreen"
+        component={Komoditas}
+        options={{ title: "Pilih Komoditas" }}
+      />
+      <Stack.Screen
+        name="BuyerScreen"
+        component={Buyer}
+        options={{ title: "Data Pemesan" }}
+      />
+      <Stack.Screen
+        name="SummaryScreen"
+        component={Summary}
+        options={{ title: "Review Pesanan" }}
+      />
+      <Stack.Screen
+        name="OrderSuccessScreen"
+        component={OrderSuccess}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PestisidaScreen"
+        component={Pestisida}
+        options={{ title: "Pilih Cairan" }}
+      />
+    </Stack.Navigator>
   );
 };
 
